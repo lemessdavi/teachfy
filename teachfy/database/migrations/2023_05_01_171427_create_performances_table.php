d9969a959f93dd8de8a728dcfac458b82633fad5
Davi Lemes <ndlemes7@outlook.com>
2023-05-01 18:04:00 -0300

    performance table - up function created

--- teachfy/database/migrations/2023_05_01_171427_create_performances_table.php
+++ teachfy/database/migrations/2023_05_01_171427_create_performances_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Card;
 
 return new class extends Migration
 {
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('performances', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Card::class);
+            $table->foreignIdFor(User::class);
+            $table->smallInteger('performance'); //maybe change the name of this column
+
             $table->timestamps();
         });
     }
