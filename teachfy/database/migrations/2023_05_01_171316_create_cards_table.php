f13e6aa425ef7d4ddcef8fc70bf5ec5da02d3707
Davi Lemes <ndlemes7@outlook.com>
2023-05-01 17:57:01 -0300

    card table - up function created

--- teachfy/database/migrations/2023_05_01_171316_create_cards_table.php
+++ teachfy/database/migrations/2023_05_01_171316_create_cards_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Deck;
 
 return new class extends Migration
 {
@@ -13,6 +14,17 @@ return new class extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
+
+            $table->bigInteger('deck_id')->unsigned();
+            $table->smallInteger('deck_type');
+            $table->foreign(['deck_id', 'deck_type'])->references(['id', 'type'])->on('decks');
+
+            $table->smallInteger('type'); //add check constraint [0,1] 
+            $table->text('question');
+            $table->text('answer')->nullable();
+            $table->decimal('points', $precision = 5, $scale = 2 ); //this column name needs to be changed
+
+            
             $table->timestamps();
         });
     }
