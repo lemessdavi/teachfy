84476bddc4b377e633cfca64ba70806ac1523f19
Davi Lemes <ndlemes7@outlook.com>
2023-05-07 22:29:53 -0300

    package fix

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Participant extends Model
 {
     use HasFactory;
+
+
+    public function users(){
+        return $this->belongsTo('App\Models\User');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Participant;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/paticipants', function () {
+   
+    $participant = Participant::find(1);
+
+    // foreach ($participant->users as $user) {
+    //     echo $user->name . "<br>";
+    // }
+
+    return $participant->users->name;
 });
