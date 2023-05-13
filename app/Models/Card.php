a756aef1ac833b9e05cd9ab6946b086f197d9c74
Davi Lemes <ndlemes7@outlook.com>
2023-05-12 21:31:47 -0300

    card relation with deck

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Card extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'deck_id',
+        'deck_type',
+        'type',
+        'question',
+        'answer',
+        'points'
+    ];
+    
+    public function deck():BelongsTo{
+        return $this->belongsTo(Deck::class);
+    }
+
 }
