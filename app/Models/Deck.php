02b8e9dfdb8b47f95d2c5f698e61318c75888b84
Davi Lemes <ndlemes7@outlook.com>
2023-05-12 21:17:30 -0300

    deck relation with user and folder

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -4,8 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Deck extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id', #deck creator
+        'folder_id',
+        'name',
+        'public',
+        'clonable',
+        'feedback',
+        'type'
+        //maybe the index?
+    ];
+
+    public function user():BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function folder():BelongsTo{
+        return $this->belongsTo(Folder::class);
+    }
 }
