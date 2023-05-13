96677875417996261940df217afdb9cbe4cbab85
Davi Lemes <ndlemes7@outlook.com>
2023-05-12 21:35:55 -0300

    option relation with card

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Option extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'card_id',
+        'description',
+        'correct_answer'
+    ];
+    
+    public function card():BelongsTo{
+        return $this->belongsTo(Card::class);
+    }
 }
