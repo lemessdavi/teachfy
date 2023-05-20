976265d09018a9a1b03c867cea6fdd9f1a9e593c
Davi Lemes <ndlemes7@outlook.com>
2023-05-20 18:59:11 -0300

    deck relation with participants and cards

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Deck extends Model
 {
@@ -28,5 +29,13 @@ class Deck extends Model
 
     public function folder():BelongsTo{
         return $this->belongsTo(Folder::class);
+    }
+
+    public function participants():HasMany{
+        return $this->hasMany(Participant::class);
+    }
+
+    public function cards():HasMany{
+        return $this->hasMany(Card::class);
     }
 }
