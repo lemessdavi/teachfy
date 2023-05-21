aef779d96e3339f1763630420d0fc9b0a52363df
Davi Lemes <ndlemes7@outlook.com>
2023-05-21 15:37:01 -0300

    route for user CRUD methods testing

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Models\Participant;
+use GuzzleHttp\Psr7\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +27,5 @@ Route::get('/paticipants/{id}', function ($id) {
 
     return $participant->user->name;
 });
+
+Route::resource('users', UserController::class);
