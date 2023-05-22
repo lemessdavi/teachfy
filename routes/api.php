e1f7f07106a2491ddeb27600379031db23751c65
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-05-22 20:51:15 -0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DeckController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('decks', DeckController::class);
+
+Route::resource('users', UserController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
