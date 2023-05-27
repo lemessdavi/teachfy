5b20e935d71a8473c81a577a6f6403a767941abe
Davi Lemes <ndlemes7@outlook.com>
2023-05-27 15:35:11 -0300

    CRUD methods implemented

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\DeckStoreRequest;
+use App\Models\Folder;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class FolderController extends Controller
+{
+    public function store(DeckStoreRequest $request): JsonResponse
+    {
+        try {
+            DB::beginTransaction();
+
+            $folder = new Folder();
+            $folder->fill($request->all());
+            $folder->save();
+
+            DB::commit();
+            return response()->json(['message' => 'Registro salvo com sucesso', 'data' => $folder]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage());//deverá haver tratamento de exception
+        }
+    }
+
+    public function show(string $id)
+    {
+        //
+        try {
+            $folder = Folder::findOrFail($id);
+
+            return response()->json(['message' => 'Registro: ', 'data' => $folder]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): JsonResponse
+    {
+        //
+        try {
+            DB::beginTransaction();
+
+            $folder = Folder::findOrFail($id);
+            $folder->fill($request->all());
+            $folder->save();    
+
+            DB::commit();
+            return response()->json(['message' => 'Registro alterado com sucesso', 'data' => $folder]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage());
+        }
+    }
+}
