83527f14c1b5906a6d06e1fc7d712924d6c1a256
Davi Lemes <ndlemes7@outlook.com>
2023-05-27 15:23:59 -0300

    update and show method added

--- app/Http/Controllers/DeckController.php
+++ app/Http/Controllers/DeckController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\DeckStoreRequest;
 use App\Models\Deck;
 use Exception;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class DeckController extends Controller {
@@ -25,6 +26,40 @@ class DeckController extends Controller {
         } catch (Exception $e) {
             DB::rollBack();
             throw new Exception($e->getMessage());//deverá haver tratamento de exception
+        }
+    }
+
+    public function show(string $id)
+    {
+        //
+        try {
+            $deck = Deck::findOrFail($id);
+
+            return response()->json(['message' => 'Registro alterado com sucesso', 'data' => $deck]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id): JsonResponse
+    {
+        //
+        try {
+            DB::beginTransaction();
+
+            $deck = Deck::findOrFail($id);
+            $deck->fill($request->all());
+            $deck->save();    
+
+            DB::commit();
+            return response()->json(['message' => 'Registro alterado com sucesso', 'data' => $deck]);
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage());
         }
     }
 }
