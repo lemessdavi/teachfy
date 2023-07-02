bbb9df7077a43a068cb5bcc1b13242eb10d6c28b
Davi Lemes <ndlemes7@outlook.com>
2023-07-02 15:44:16 -0300

    autogenerate route fix

--- routes/web.php
+++ routes/web.php
@@ -4,35 +4,42 @@ use Illuminate\Support\Facades\Route;
 use OpenAI as GlobalOpenAI;
 use OpenAI\Laravel\Facades\OpenAI;
 
-Route::get('/ask/{n}/{prompt}', function ($n, $prompt) {
+Route::get('/ask/{n}/{prompt}/{type}', function ($n, $prompt, $type) {
 
-    if( 1 == 1){ //
+    if($type == 1 ){ //objetiva
         $options = ', toda questao deve ser objetiva, contendo 4 opcoes de resposta';
+        $answer  =  ', por fim me indique também qual é a resposta correta para a pergunta';
+        $json = 'Apresente o resultado em formato JSON conforme exemplo abaixo 
+        {
+            "question": teste,
+            "answers": [
+              {
+                "description": "teste1",
+                "isCorrect": 0
+              },
+              {
+                "description": "teste2",
+                "isCorrect": 1
+              }
+            ]
+          }';
     };
 
-    if( 1 == 1){ //
+    if($type == 2){ //dissertativa and anki
         $answer = ', por fim me indique também qual é a resposta correta para a pergunta';
+        $json = 'Apresente o resultado em formato JSON conforme exemplo abaixo 
+        {
+          "question": teste,
+          "answer": resposta teste
+        }';
     };
 
     $result = OpenAI::completions()->create([
         'model' => 'text-davinci-003',
         'prompt' => 'O texto a seguir é um tema fornecido pelo usuário, formule '. $n .' pergunta sobre o tema '.  $prompt.
-        'retorne elas para mim em formato de JSON, separando cada pergunta, sempre chame cada uma delas de (pergunta + numero da pergunta)'. $options. $answer   ,
+        $options . $answer . $json ,
         'max_tokens' => 3500,
     ]);
-
-    echo $result['choices'][0]['text']; // an open-source, widely-used, server-side scripting language.
-});
-
-//com gabarito junto
-Route::get('/ask2/{n}/{prompt}', function ($n, $prompt) {
-
-    $result = OpenAI::completions()->create([
-        'model' => 'text-davinci-003',
-        'prompt' => 'O texto a seguir é um tema fornecido pelo usuário, formule '. $n .' pergunta sobre o tema '.  $prompt.
-        'retorne elas para mim em formato de JSON, separando cada pergunta, sempre chame cada uma delas de (pergunta + numero da pergunta)',
-        'max_tokens' => 500,
-    ]);
-
+     
     echo $result['choices'][0]['text']; // an open-source, widely-used, server-side scripting language.
 });
