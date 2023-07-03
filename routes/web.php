7fd8b125bc6450a1ecefd81fdd81e823bfa3173f
Davi Lemes <ndlemes7@outlook.com>
2023-07-02 21:51:48 -0300

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use OpenAI\Laravel\Facades\OpenAI;
 
 Route::get('/ask/{n}/{prompt}/{type}', function ($n, $prompt, $type) {
 
+  $options = '';
+
     if($type == 1 ){ //objetiva
         $options = ', toda questao deve ser objetiva, contendo 4 opcoes de resposta';
         $answer  =  ', por fim me indique também qual é a resposta correta para a pergunta';
