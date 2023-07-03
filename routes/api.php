eb6857d490327cd85940a44941dfe2c4cb35668d
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-07-03 18:34:52 -0300

    add route chatgpt into api middleware

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CommunityController;
 use App\Http\Controllers\DeckController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use OpenAI\Laravel\Facades\OpenAI;
 
 Route::post('/login', [LoginController::class, 'login']);
 
@@ -20,4 +21,56 @@ Route::group(['middleware' => ['auth.react']], function () {
     Route::apiResource('decks', DeckController::class);
 
     Route::get('/generate', [AutomaticCardCreationController::class, 'create']);
+
+
+    Route::get('/ask/{n}/{prompt}/{type}', function ($n, $prompt, $type) {
+
+        $options = '';
+        $answer = '';
+        $json = '';
+
+        if ($type == 1) { // Objetiva
+            $options = ', toda questao deve ser objetiva, contendo 4 opcoes de resposta';
+            $answer = ', por fim me indique também qual é a resposta correta para a pergunta';
+            $json = 'Apresente o resultado em formato JSON conforme exemplo abaixo, SUA RESPOSTA DEVE SER SOMENTE UM JSON, NADA MAIS
+            [{
+            "question": teste,
+            "answers": [
+                {
+                "description": "teste1",
+                "isCorrect": 0
+                },
+                {
+                "description": "teste2",
+                "isCorrect": 1
+                }
+            ]
+            }]';
+        }
+
+        if ($type == 2) { // Dissertativa and Anki
+            $answer = ', por fim me indique também qual é a resposta correta para a pergunta';
+            $json = 'Apresente o resultado em formato JSON conforme exemplo abaixo, SUA RESPOSTA DEVE SER SOMENTE UM JSON, NADA MAIS
+            [{
+            "question": teste,
+            "answer": resposta teste
+            }
+            }]
+            ';
+        }
+
+        $result = OpenAI::completions()->create([
+            'model' => 'text-davinci-003',
+            'prompt' => 'O texto a seguir é um tema fornecido pelo usuário, formule ' . $n . ' pergunta sobre o tema ' . $prompt .
+                $options . $answer . $json,
+            'max_tokens' => 3500,
+        ]);
+
+        $response = [
+            'data' => json_decode($result['choices'][0]['text'])
+        ];
+
+        return response()->json($response);
+    });
+
 });
